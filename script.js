88091f84fbf769a9bd338f3da28e3cde217ce097
Mohammad Khreiss <mhamad.a.khreiss@gmail.com>
2023-01-07 20:11:19 +0200

    added rainbow colors

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const container = document.querySelector('#container')
-container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-container.style.gridTemplateRows = `repeat(16, 1fr)`;
-for(let i=0;i<16*16;i++){
-    
-    const square = document.createElement('div')
-    square.classList.add('square')
-    square.addEventListener('mouseover',color)
-    container.appendChild(square)
+function createCanvas(){
+    container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+    container.style.gridTemplateRows = `repeat(16, 1fr)`;
+    for(let i=0;i<16*16;i++){
+        
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.addEventListener('mouseover',color)
+        container.appendChild(square)
+    }
+}
+const randColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
 function color(e){
-    e.target.style.backgroundColor = 'black'
+    let color = randColor();
+    e.target.style.backgroundColor = color
 }
+createCanvas()
