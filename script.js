54478b1c86c6011f0b300510181788bc81a2d863
Mohammad Khreiss <mhamad.a.khreiss@gmail.com>
2023-04-12 18:17:51 +0300

    added icon and styling and hover or click to draw option

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //default values
 const DEFAULT_COLOR = '#000000';
 const DEFAULT_MODE = 'rainbow';
-const DEFAULT_SIZE = 16;
+const DEFAULT_SIZE = 24;
+const DEFAULT_DRAW = 'mouseover';
 
 let currColor = DEFAULT_COLOR;
 let currMode = DEFAULT_MODE;
 let currSize = DEFAULT_SIZE;
+let currDraw = DEFAULT_DRAW;
 
 //setters functions
 function setCurrentColor(color){
@@ -20,6 +22,14 @@ function setCurrentSize(size){
     currSize = size;
 }
 
+function toggleDraw(){
+    if(currDraw === 'mouseover')
+        currDraw = 'click'
+    else
+        currDraw = 'mouseover'
+    reloadCanvas()
+}
+
 //getting elements
 const container = document.getElementById('container');
 const colorInput = document.getElementById('colorInput');
@@ -29,6 +39,7 @@ const eraserBtn = document.getElementById('eraseBtn');
 const clearBtn = document.getElementById('clearBtn');
 const sizeText = document.getElementById('size');
 const slider = document.getElementById('slider');
+const checkbox = document.getElementById('checkbox');
 
 //setting events
 colorInput.oninput = (e) => setCurrentColor(e.target.value);
@@ -38,6 +49,10 @@ eraserBtn.onclick = () => setCurrentMode('erase');
 clearBtn.onclick = () => reloadCanvas();
 slider.onmousemove = (e) => updateSizeText(e.target.value);
 slider.onchange = (e) => changeSize(e.target.value);
+checkbox.onchange = () => toggleDraw()
+
+
+
 
 //updating size functions
 function changeSize(value){
@@ -68,7 +83,7 @@ function createCanvas(size){
         
         const square = document.createElement('div');
         square.classList.add('square');
-        square.addEventListener('mouseover',color);
+        square.addEventListener(currDraw,color);
         container.appendChild(square)
     }
 }
@@ -82,7 +97,7 @@ function color(e){
     else if(currMode === 'color')
         color = currColor
     else if(currMode === 'erase')
-        color = '#ffffff';
+        color = '#ededed';
     e.target.style.backgroundColor = color;
 }
 
